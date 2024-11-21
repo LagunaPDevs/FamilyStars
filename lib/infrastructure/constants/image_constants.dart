d68251d46942057f474b1eafca85c613ba4f2cfc
Maria Laguna <marialagunap@gmail.com>
2024-11-21 07:39:32 +0100

    Fix assets

--- lib/infrastructure/constants/image_constants.dart
+++ lib/infrastructure/constants/image_constants.dart
@@ -1,18 +1,18 @@
 // All image constants defined here
 
 class ImageConstants {
-  static const String logoFamilyStars = 'images/familystarslogo.png';
-  static const String logoKids = 'images/kidslogo.png';
-  static const String logoParents = 'images/parentslogo.png';
-  static const String simpleStar = 'images/simplestar.png';
-  static const String groceryIcon = 'images/groceryicon.png';
-  static const String homeIcon = 'images/homeicon.png';
-  static const String schoolIcon = 'images/schoolicon.png';
-  static const String softchangingLogo = 'images/logosoftchanging.png';
+  static const String logoFamilyStars = 'assets/images/familystarslogo.png';
+  static const String logoKids = 'assets/images/kidslogo.png';
+  static const String logoParents = 'assets/images/parentslogo.png';
+  static const String simpleStar = 'assets/images/simplestar.png';
+  static const String groceryIcon = 'assets/images/groceryicon.png';
+  static const String homeIcon = 'assets/images/homeicon.png';
+  static const String schoolIcon = 'assets/images/schoolicon.png';
+  static const String softchangingLogo = 'assets/images/logosoftchanging.png';
 
   // Social media
-  static const String facebookIcon = 'images/facebookicon.png';
-  static const String googleIcon = 'images/googleicon.png';
-  static const String twitterIcon = 'images/twittericon.png';
-  static const String userIcon = 'images/user.png';
+  static const String facebookIcon = 'assets/images/facebookicon.png';
+  static const String googleIcon = 'assets/images/googleicon.png';
+  static const String twitterIcon = 'assets/images/twittericon.png';
+  static const String userIcon = 'assets/images/user.png';
 }
